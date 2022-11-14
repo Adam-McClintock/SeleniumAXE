06b02ce225064fb10baa4d1d74ea6d06654c61f2
AMAC152 <adammcclintock@live.co.uk>
2022-11-14 13:39:18 +0000

    Tags Added

--- SeleniumAXE/Features/Employers.feature
+++ SeleniumAXE/Features/Employers.feature
@@ -12,10 +12,14 @@ Background:
 Scenario: Navigate to Hire an Apprentice Page
 	Then a Hire an Apprentice page is displayed
 
+	@UI
+	@Employers
 Scenario: Navigate to Create Advert
 	When I navigate to the Create Your Advert page
 	Then I am taken to the GOV UK Create Advert page
 
+	@UI
+	@Employers
 Scenario: Navigate to Employer Interest Page
 	When I navigate to the Employer Interest Page
 	Then a Employer Interest Page is displayed
--- SeleniumAXE/Settings.Designer.cs
+++ SeleniumAXE/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace SeleniumAXE {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool Headless {
             get {
                 return ((bool)(this["Headless"]));
